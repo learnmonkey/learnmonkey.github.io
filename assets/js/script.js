c398c6d9d58d086d3414c182be4ffe72994893d2
MonkeyED <95662308+MonkeyED@users.noreply.github.com>
2022-01-04 15:27:35 -0700

    Delete Unneeded Old Dropdown JS Code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,1 @@
-function showDropdownNav() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
+// Javascript Will Go Here
