e66490f54864a2814f5926b2018e7adc19bd0317
calgary34 <aaron34weston@gmail.com>
2022-01-03 20:49:02 -0700

    load navbar and footer using jQuery

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(function() {
+        $("#nav").load("/assets/html/nav.html");
+    });
+    $(function() {
+        $("#footer").load("/assets/html/footer.html");
+    });
+  });
