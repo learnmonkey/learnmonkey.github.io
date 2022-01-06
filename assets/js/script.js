206a68305c324b38259e2d9fa7e938a721673074
calgary34 <51468959+calgary34@users.noreply.github.com>
2022-01-06 12:45:56 -0700

    remove annoying alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,1 @@
-window.alert("Most of Learnmonkey is still under construction. If something weird happens, make sure to go on the GitHub repo and submit an issue.")
+//window.alert("Most of Learnmonkey is still under construction. If something weird happens, make sure to go on the GitHub repo and submit an issue.")
