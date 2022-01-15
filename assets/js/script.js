ff2a5cbb58f523c6e06cd24acc246d7398cd931c
The Peeps191 <thepeeps.wang@gmail.com>
2022-01-15 15:53:13 -0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ function switchDark() {
 	// }
 	var style = document.createElement("style");
 	document.head.appendChild(style);
-	var css = `a {color: yellow; text-decoration: none}
+	var css = `a {color: yellow; text-decoration: none;}
 a:hover {color: white; text-decoration: none;}
 body {background-color: black;}`
 	try {
