4b3f62e8b721c2364eb7a945a38969a4bcae2adf
The Peeps191 <thepeeps.wang@gmail.com>
2022-01-15 16:38:44 -0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,19 @@ function switchDark() {
 	// for(var n = 0; n < allLinks ; n++) {
 	// 	document.allLinks [n].style.cursor = "wait";
 	// }
-	var style = document.createElement("style");
-	style.setAttribute("id", "dark-style");
-	document.head.appendChild(style);
-	var css = `a {color: yellow; text-decoration: none;}
-a:hover {color: lightgrey; text-decoration: underline;}
-body {background-color: black; font: white; color: white;}`
-	try {
-		style.innerHTML = css;
-	}
-	catch (_ie) {
-		style.styleSheet.cssText = css;
+	if (document.getElementById("dark-style") != null) {
+		var style = document.createElement("style");
+		style.setAttribute("id", "dark-style");
+		document.head.appendChild(style);
+		var css = `a {color: yellow; text-decoration: none;}
+	a:hover {color: lightgrey; text-decoration: underline;}
+	body {background-color: black; font: white; color: white;}`
+		try {
+			style.innerHTML = css;
+		}
+		catch (_ie) {
+			style.styleSheet.cssText = css;
+		}
 	}
 }
 
