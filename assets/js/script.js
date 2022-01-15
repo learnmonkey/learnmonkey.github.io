a7eed4c8d85118c439bc2b6f8f1f90386f583d6d
The Peeps191 <thepeeps.wang@gmail.com>
2022-01-15 15:25:56 -0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-hljs.highlightAll();
+hljs.highlightAll(); // Code Highlighting
+
+// Light And Dark Mode
+function switchDark() {
+	// var allLinks = document.links || document.getElementsByTagName('a');
+	// for(var n = 0; n < allLinks ; n++) {
+	// 	document.allLinks [n].style.cursor = "wait";
+	// }
+	var style = document.createElement("style");
+	document.head.appendChild(style);
+	try {
+		style.innerHTML = `a { cursor: wait; }`;
+	}
+	catch (_ie) {
+		style.styleSheet.cssText = `a { cursor: wait; }`;
+	}
