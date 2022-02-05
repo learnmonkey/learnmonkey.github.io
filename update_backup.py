00630dc770425e336f062c7bb8fd145c7a1e24b5
calgary34 <51468959+calgary34@users.noreply.github.com>
2022-02-05 15:43:01 -0700

    Delete update_backup.py
    The update backup stuff was put in the commit.py file

--- update_backup.py
+++
@@ -1,9 +0,0 @@
-# Updates the backup branch
-# python update_backup.py
-
-import os
-os.system("git pull")
-os.system("git checkout backup")
-os.system("git merge main")
-os.system("git push")
-os.system("git checkout main")
