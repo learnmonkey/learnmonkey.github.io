14f17723feeaab7338698ea60c6006b92bf86be3
The Peeps191 <thepeeps.wang@gmail.com>
2022-02-05 15:10:52 -0700

    quick reword

--- commit.py
+++ commit.py
@@ -1,6 +1,6 @@
 # You can use this file to more easily commit to Learnmonkey if you are using the command line.
 # Use: python commit.py [your commit message here]
-# We'll make it a bit complex soon...
+# We'll make it into a CLI later...
 
 import os
 import sys
