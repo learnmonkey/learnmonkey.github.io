d3f332bf75019d7a5bfc19db1c8fca1814640956
The Peeps191 <thepeeps.wang@gmail.com>
2022-02-05 19:35:34 -0700

    quick comments

--- commit.py
+++ commit.py
@@ -5,12 +5,12 @@
 
 import os
 import sys
-del sys.argv[0]
-os.system("git pull")
-os.system("git add -A")
-os.system(f"""git commit -m "{' '.join(sys.argv)}" -- .""")
-os.system("git push")
-os.system("git checkout backup")
-os.system("git merge main")
-os.system("git push")
-os.system("git checkout main")
+del sys.argv[0] # the text "commit.py" is part of argv
+os.system("git pull") # update local clone
+os.system("git add -A") # add changes
+os.system(f"""git commit -m "{' '.join(sys.argv)}" -- .""") # commit code
+os.system("git push") # push local commits
+os.system("git checkout backup") # switch to backup branch
+os.system("git merge main")# update backup with main
+os.system("git push")# push new backup branch with changes
+os.system("git checkout main") # switch back to main branch
